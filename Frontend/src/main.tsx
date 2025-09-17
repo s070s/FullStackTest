d6d550baa4977f4aa4f825698d9c7081eb6b1b79
George Sotiropoulos <gsotiro@hotmail.com>
2025-09-17 12:19:58 +0300

    Add comment to clarify StrictMode behavior in development

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App";
 import { AuthProvider } from "./utils/contexts/AuthContext";
 import { DeviceProvider } from "./utils/contexts/DeviceContext";
 import "./index.css";
-
+//StrictMode causes useEffect and therefore api calls to run twice in dev mode only
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <AuthProvider>
